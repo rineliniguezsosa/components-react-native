e31e59ec3d02ccf013c9950720cd7145f748340e
Rinel <rineliniguezsosa@gmail.com>
2024-10-05 18:14:16 -0600

    [FEAT]  Estableciendo la navegación de tipo stack en el archivo App.tsx
    - estableciendo el component StackNavigator dentro del NavigationContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './navigation/StackNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Text>ComponentName</Text>
+      <StackNavigator/>
     </NavigationContainer>
   );
 };
