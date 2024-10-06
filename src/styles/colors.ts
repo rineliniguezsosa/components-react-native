4ec0adc296236d0079b4001dbb90c3c85bc3ca20
Rinel <rineliniguezsosa@gmail.com>
2024-10-05 18:27:21 -0600

    [FEAT] Creando la interface ThemeColors y el objeto llamado color con los colores por compartir

---
+++ src/styles/colors.ts
@@ -0,0 +1,15 @@
+export interface ThemeColors {
+    primary: string;
+    text: string;
+    background: string;
+    cardBackground: string;
+    buttonTextColor: string;
+  }
+
+export const colors: ThemeColors = {
+    primary: '#5856D6',
+    text: 'black',
+    background: '#F3F2F7',
+    cardBackground: 'white',
+    buttonTextColor: 'white',
+};
