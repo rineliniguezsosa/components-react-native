df664434f446d6c1c20a32b079f170e797506a21
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 22:45:36 -0600

    [FEAT] Creeando el funcional component Animation102Screen

---
+++ src/screens/animations/Animation102Screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const Animation102Screen = () => {
+  return (
+    <View>
+      <Text>Animation102Screen</Text>
+    </View>
+  );
+};
