cc17d73982eb90fe2fa613a26d537c8f31fdd089
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 22:47:53 -0600

    [FEAT] Cambios en el archivo Animation102Screen
    - importando StyleSheet
    - estableciendo los estilo de la clase container
    - creando el objeto styles con la clase container y box

--- src/screens/animations/Animation102Screen.tsx
+++ src/screens/animations/Animation102Screen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text, StyleSheet } from 'react-native';
 
 export const Animation102Screen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Animation102Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    box: {
+      backgroundColor: '#61dafb',
+      width: 80,
+      height: 80,
+      borderRadius: 4,
+    },
+});
