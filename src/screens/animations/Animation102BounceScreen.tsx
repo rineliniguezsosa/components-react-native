41bf80b41c740601a46a22f94f31b6a6efb77e3b
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 20:27:18 -0600

    [FEAT] Multiples cambios en el archivo Animation102BounceScreen
    - importando la animación Easing
    - estableciendo la variable boxAnim con el useRef para establecer una posición de la caja
    - estableciendo la animación para mover el elemento a 200 unidades y estableciendo al a imación bounce
    - restableciendo las animaciones en el método fadeOut
    - moviendo la caja mediante la prop transform

--- src/screens/animations/Animation102BounceScreen.tsx
+++ src/screens/animations/Animation102BounceScreen.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useRef } from 'react';
-import { View,Text,StyleSheet,Pressable, Animated } from 'react-native';
+import { View,Text,StyleSheet,Pressable, Animated, Easing } from 'react-native';
 import { colors } from '../../styles/colors';
 
 export const Animation102BounceScreen = () => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
+  const boxAnim = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () =>{
+    Animated.timing(boxAnim,{
+        toValue:200,
+        duration:700,
+        useNativeDriver:true,
+        easing:Easing.bounce,
+    }).start(()=> console.log('yes'));
+
     Animated.timing(fadeAnim,{
       toValue:1,
       duration:300,
@@ -20,6 +28,8 @@ export const Animation102BounceScreen = () => {
       duration:300,
       useNativeDriver:true,
     }).start(()=> console.log('yes'));
+
+    boxAnim.resetAnimation();
   };
 
   return (
@@ -27,7 +37,7 @@ export const Animation102BounceScreen = () => {
       <Text>Animation101Screen</Text>
 
       <Animated.View
-        style={[styles.purpleBox,{opacity:fadeAnim}]}
+        style={[styles.purpleBox,{opacity:fadeAnim,transform:[{translateY:boxAnim}]}]}
       />
 
       <Pressable style={{marginTop:50}} onPress={fadeIn}><Text>Fedin</Text></Pressable>
