6ce6157aa1ecd5a92db5d974174f793e041cdf8a
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 19:13:55 -0600

    [FEAT] Importando el Pressable

--- src/screens/animations/Animation101Screen.tsx
+++ src/screens/animations/Animation101Screen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View,Text,StyleSheet } from 'react-native';
+import { View,Text,StyleSheet,Pressable } from 'react-native';
 import { colors } from '../../styles/colors';
 
 export const Animation101Screen = () => {
