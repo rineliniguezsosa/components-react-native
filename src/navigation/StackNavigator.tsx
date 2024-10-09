d849b93e44e48a5ecfe0fefd7f4e1808057680ba
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 18:57:02 -0600

    [FEAT] Cambios en el archivo StackNavigator
    - importando el funcional component Animation101Screen
    - estableciendo la prop initialRouteName en el Stack.Navigator para mostrar el component HomeScreen
    - estableciendo la ruta Animation101Screen en el stack navigator para mostrar el contenido

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/home/Homescreen';
+import { Animation101Screen } from '../screens/animations/Animation101Screen';
 
 const Stack = createStackNavigator();
 
@@ -10,8 +11,10 @@ export const StackNavigator = () => {
         screenOptions={{
             headerShown:false,
         }}
+        initialRouteName="HomeScreen"
     >
       <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+      <Stack.Screen name="Animation101Screen" component={Animation101Screen}/>
     </Stack.Navigator>
   );
 };
