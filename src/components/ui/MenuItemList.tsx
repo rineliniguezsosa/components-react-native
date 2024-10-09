27bf9c57726bf7938ed17aa7041043f43069d6ca
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 19:02:29 -0600

    [FEAT] Cambios en el MenuItemList
    - importando el useNavigation
    - creando la variable navigation donde le pasamos la referencia de la función useNavigation y tipamos la rutas por any por el momento
    - estableciendo un fragment como envoltura del componente MenuItemList
    - cambiando la referencia del log en el método onPress por el navigate para poder navegar hacia las vistas del stack

--- src/components/ui/MenuItemList.tsx
+++ src/components/ui/MenuItemList.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { View,Text,Pressable,StyleSheet } from 'react-native';
 import { colors } from '../../styles/colors';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
 
 interface Props {
     name:string,
@@ -11,14 +12,19 @@ interface Props {
 }
 
 export const MenuItemList = ({name,icon,component}:Props) => {
+  const navigation = useNavigation<any>();
+    console.log(component);
+
   return (
-    <Pressable onPress={()=> console.log('items')}>
+    <>
+    <Pressable onPress={()=> navigation.navigate(component)}>
       <View style={{...styles.constainer,backgroundColor:colors.background}}>
         <Icon name={icon} size={25} style={{marginRight:10,color:colors.primary}}/>
         <Text style={{color:colors.text}}>{name}</Text>
-        <Icon name="chevron-forward-outline" size={25} style={{marginRight:10,color:colors.primary}}/>
+        <Icon name="chevron-forward-outline" size={25} style={{marginLeft:'auto',color:colors.primary}}/>
       </View>
     </Pressable>
+    </>
   );
 };
 
