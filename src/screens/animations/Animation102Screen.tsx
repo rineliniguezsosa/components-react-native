625d9a94638d43ba98e19c8f347c962bdfe54ac4
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 22:57:31 -0600

    [FEAT] Replicando el ejemplo de la documentación oficial

--- src/screens/animations/Animation102Screen.tsx
+++ src/screens/animations/Animation102Screen.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import { View,Text, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View,StyleSheet, Animated,PanResponder } from 'react-native';
 
 export const Animation102Screen = () => {
+    const pan = useRef(new Animated.ValueXY()).current;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: Animated.event([
+          null,
+          {
+            dx: pan.x, // x,y are Animated.Value
+            dy: pan.y,
+          },
+        ],{useNativeDriver:false}),
+        onPanResponderRelease: () => {
+          Animated.spring(
+            pan, // Auto-multiplexed
+            {toValue: {x: 0, y: 0}, useNativeDriver: false}, // Back to zero
+          ).start();
+        },
+      });
   return (
     <View style={styles.container}>
-      <Text>Animation102Screen</Text>
+      <Animated.View style={[pan.getLayout(),styles.box]} {...panResponder.panHandlers} />
     </View>
   );
 };
