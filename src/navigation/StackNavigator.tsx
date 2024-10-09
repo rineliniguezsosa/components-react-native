12350063c3c61621f79651dfc42362280d476222
Rinel <rineliniguezsosa@gmail.com>
2024-10-09 17:59:37 -0600

    [FEAT] Estableciendo en el Stack Navigator la ruta para acceder al componente SwicthScreen

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -4,6 +4,7 @@ import { HomeScreen } from '../screens/home/Homescreen';
 import { Animation101Screen } from '../screens/animations/Animation101Screen';
 import { Animation102BounceScreen } from '../screens/animations/Animation102BounceScreen';
 import { Animation102Screen } from '../screens/animations/Animation102Screen';
+import { SwicthScreen } from '../screens/switches/SwitchScreen';
 
 const Stack = createStackNavigator();
 
@@ -19,6 +20,7 @@ export const StackNavigator = () => {
       <Stack.Screen name="Animation101Screen" component={Animation101Screen}/>
       <Stack.Screen name="Animation102BounceScreen" component={Animation102BounceScreen}/>
       <Stack.Screen name="Animation102Screen" component={Animation102Screen}/>
+      <Stack.Screen name="SwicthScreen" component={SwicthScreen}/>
     </Stack.Navigator>
   );
 };
