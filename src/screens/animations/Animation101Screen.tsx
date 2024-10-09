b5b8dea7b5f5609b8a983612083901fdbfeb98f8
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 19:11:01 -0600

    [FEAT] Multiples cambios en el archivo Animation101Screen
    - importando StyleSheet
    - importando colors
    - creando el objeto styles para establecer el css, creando la clase container con las props flex;1, justify y alignItems en center, creando la clase purpleBox con el background colors.primary y un width y un height de 150
    - estableciendo la clase container en el View principal

--- src/screens/animations/Animation101Screen.tsx
+++ src/screens/animations/Animation101Screen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,StyleSheet } from 'react-native';
+import { colors } from '../../styles/colors';
 
 export const Animation101Screen = () => {
   return (
-    <View>
+    <View style={styles.constainer}>
       <Text>Animation101Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  constainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  purpleBox:{
+    backgroundColor:colors.primary,
+    width:150,
+    height:150,
+  },
+});
