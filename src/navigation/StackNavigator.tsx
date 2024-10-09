749489f88c97a7dce4ee50b49c8f503265ad7215
Rinel <rineliniguezsosa@gmail.com>
2024-10-08 20:23:31 -0600

    [FEAT] Cambios en el stack navigator
    - importando Animation102BounceScreen
    - estableciendo la ruta "Animation102BounceScreen" en el stack navigator para poder observar la vista del componente

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/home/Homescreen';
 import { Animation101Screen } from '../screens/animations/Animation101Screen';
+import { Animation102BounceScreen } from '../screens/animations/Animation102BounceScreen';
 
 const Stack = createStackNavigator();
 
@@ -15,6 +16,7 @@ export const StackNavigator = () => {
     >
       <Stack.Screen name="HomeScreen" component={HomeScreen}/>
       <Stack.Screen name="Animation101Screen" component={Animation101Screen}/>
+      <Stack.Screen name="Animation102BounceScreen" component={Animation102BounceScreen}/>
     </Stack.Navigator>
   );
 };
