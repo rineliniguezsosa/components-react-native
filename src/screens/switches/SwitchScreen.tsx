87b263835583dbea3e9329358eb2e4b638229bda
Rinel <rineliniguezsosa@gmail.com>
2024-10-09 18:29:31 -0600

    [FEAT] Cambios en el component SwicthScreen
    - importando el component Card
    - en el componente CustomView establecemos un paddingHorizontal de 10
    - establecemos el component Card con el contenido del nombre del componente

--- src/screens/switches/SwitchScreen.tsx
+++ src/screens/switches/SwitchScreen.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
+import { Card } from '../../components/ui/Card';
 
 export const SwicthScreen = () => {
   return (
-    <CustomView style={{marginTop:100}}>
-      <Text>SwicthScreen</Text>
+    <CustomView style={{marginTop:100,paddingHorizontal:10}}>
+      <Card><Text>SwicthScreen</Text></Card>
     </CustomView>
   );
 };
