199195b14bd922335ad3b5b7b332873eacb4a287
Rinel <rineliniguezsosa@gmail.com>
2024-10-11 17:33:04 -0600

    [FEAT] Multiples cambios en el component SwitchScreen
    - eliminamos la importación de Button
    - importando el useState
    - importando el component Switch de react native
    - cambiando el nombre del funcional component SwicthScreen por SwitchScreen
    - creamos un state isEnabled inicializado en false
    - creamos la función toggleSwitch para moficicar el state
    - pegamos el contenido de Switch de la documentación oficial de react native

--- src/screens/switches/SwitchScreen.tsx
+++ src/screens/switches/SwitchScreen.tsx
@@ -1,15 +1,23 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Switch } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
 import { Card } from '../../components/ui/Card';
-import { Button } from '../../components/ui/Button';
 
-export const SwicthScreen = () => {
+export const SwitchScreen = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <CustomView style={{marginTop:100,paddingHorizontal:10}}>
-      <Card><Text>SwicthScreen</Text></Card>
-      <Button text="Click me" onPress={()=> {}}/>
+      <Card>
+      <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      </Card>
     </CustomView>
   );
 };
