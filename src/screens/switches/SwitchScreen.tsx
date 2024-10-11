73b3b2252a0b33b41ffd062ff5dea28e4383319a
Rinel <rineliniguezsosa@gmail.com>
2024-10-11 17:19:19 -0600

    [FEAT] Cambios en el componente SwitchScreen
    - importando el component Button
    - estableciendo el componente Button con la prop text igual a click me y la prop onPress con una función vacia

--- src/screens/switches/SwitchScreen.tsx
+++ src/screens/switches/SwitchScreen.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Text } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
 import { Card } from '../../components/ui/Card';
+import { Button } from '../../components/ui/Button';
 
 export const SwicthScreen = () => {
   return (
     <CustomView style={{marginTop:100,paddingHorizontal:10}}>
       <Card><Text>SwicthScreen</Text></Card>
+      <Button text="Click me" onPress={()=> {}}/>
     </CustomView>
   );
 };
