57f3e8cf6d1b786e40cbc465800f94c5e6977246
Rinel <rineliniguezsosa@gmail.com>
2024-10-11 17:41:26 -0600

    [FEAT} Cambios en el CustomSwitch
    - impprtando el StyleSheet
    - estableciendo en el componente View el style switchRow
    - creando el objeto styles donde crearemos las clases css, creé la clase switchRow donde estableci display flex con las props flexDirection:'row', justifyContent:'space-between',alignItems:'center',marginVertical:5,

--- src/components/ui/CustomSwitch.tsx
+++ src/components/ui/CustomSwitch.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text, StyleSheet } from 'react-native';
 
 export const CustomSwitch = () => {
   return (
-    <View>
+    <View style={styles.switchRow}>
       <Text>CustomSwitch</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    switchRow:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        marginVertical:5,
+    },
+});
