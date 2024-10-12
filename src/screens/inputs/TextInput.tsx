066547b4f722743eefaf42ad9357f2d8401a59b6
Rinel <rineliniguezsosa@gmail.com>
2024-10-11 21:06:00 -0600

    [FEAT] Creando el funcional component TextInput

---
+++ src/screens/inputs/TextInput.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const TextInput = () => {
+  return (
+    <View>
+      <Text>TextInput</Text>
+    </View>
+  );
+};
