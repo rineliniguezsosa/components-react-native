2bb14121aa64387db5745e429c61434a918988fb
Rinel <rineliniguezsosa@gmail.com>
2024-10-12 17:27:52 -0600

    [FEAT] Creando el funcional component ModelScreen

---
+++ src/screens/modal/ModalScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const ModalScreen = () => {
+  return (
+    <View>
+      <Text>ModalScreen</Text>
+    </View>
+  );
+};
