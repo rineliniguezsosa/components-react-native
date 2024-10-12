8188af16e7bd7bb6e50ce1b233f1e7ea41735865
Rinel <rineliniguezsosa@gmail.com>
2024-10-11 18:02:56 -0600

    [FEAT] Cambios en el SwitchScreen
    - eliminando la importación de switch
    - importando el component CustomSwitch donde pasamos las props isOn, onChange y text

--- src/screens/switches/SwitchScreen.tsx
+++ src/screens/switches/SwitchScreen.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
-import { Switch } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
 import { Card } from '../../components/ui/Card';
+import { CustomSwitch } from '../../components/ui/CustomSwitch';
 
 export const SwitchScreen = () => {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -10,12 +10,10 @@ export const SwitchScreen = () => {
   return (
     <CustomView style={{marginTop:100,paddingHorizontal:10}}>
       <Card>
-      <Switch
-        trackColor={{false: '#767577', true: '#81b0ff'}}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
+      <CustomSwitch
+        isOn={isEnabled}
+        onChange={toggleSwitch}
+        text="is Active?"
       />
       </Card>
     </CustomView>
