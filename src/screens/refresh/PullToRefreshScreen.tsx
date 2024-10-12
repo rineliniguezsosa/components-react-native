7893b750aaddc163959f9729a964544cc119db10
Rinel <rineliniguezsosa@gmail.com>
2024-10-12 16:11:35 -0600

    [FEAT] Multiples cambios en el archivo PullToRefreshScreen
    - eliminando las importaciones de View y Text
    - importando los componentes ScrollView,RefreshControl
    - importando el useSafeAreaInsets
    - importando el Title
    - importando el contenedor CustomView
    - desestructurando el top del useSafeAreaInsets
    - estableciendo el componente ScrollView  con la prop refresControl para proporcionar el componente RefresControl
    - el compoponente RefresControl establecemos las props refreshing en true para que el refresControl se muestre cargando y estableciendo la prop progressViewOffset para establecer la posición del component RefresControl
    - estableciendo el componente CustomView  y  el componente Title como un hijo
    - el componente Title tiene la prop safe y la prop text para el titulo "PullToRefreshScreen"

--- src/screens/refresh/PullToRefreshScreen.tsx
+++ src/screens/refresh/PullToRefreshScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { ScrollView,RefreshControl } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Title } from '../../components/ui/Title';
+import { CustomView } from '../../components/ui/CustomView';
 
 export const PullToRefreshScreen = () => {
+  const { top } = useSafeAreaInsets();
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={<RefreshControl refreshing={true} progressViewOffset={top}/>}
+    >
+      <CustomView>
+        <Title safe text="PullToRefreshScreen" />
+      </CustomView>
+
+    </ScrollView>
   );
 };
