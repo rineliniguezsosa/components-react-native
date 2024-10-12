8978c4559b0ffdb9eabd1e35d33a7c5bde99fbef
Rinel <rineliniguezsosa@gmail.com>
2024-10-12 17:26:27 -0600

    [FEAT] Guardando los Multiples cambios en el archivo CustomSectionListScreen

--- src/screens/sectionlist/CustomSectionListScreen.tsx
+++ src/screens/sectionlist/CustomSectionListScreen.tsx
@@ -1,10 +1,32 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View,Text } from 'react-native';
+import { Text, SectionList } from 'react-native';
+import { CustomView } from '../../components/ui/CustomView';
+import { Title } from '../../components/ui/Title';
+import { heroes } from '../../helpers/heroes';
+import { globalStyles } from '../../styles/theme';
+import { colors } from '../../styles/colors';
 
 export const CustomSectionListScreen = () => {
   return (
-    <View>
-      <Text>CustomSectionListScreen</Text>
-    </View>
+    <CustomView margin>
+      <Title safe text="CustomSectionListScreen"/>
+      <SectionList
+        sections={heroes}
+        keyExtractor={item => item}
+        renderItem={({item})=> (
+            <Text style={{marginVertical:2}}>{item}</Text>
+        )}
+        renderSectionHeader={(item) => (
+                <Text style={{...globalStyles.subTitle,backgroundColor:colors.background}}>
+                    {item.section.title}
+                </Text>
+        )}
+        stickySectionHeadersEnabled
+        showsVerticalScrollIndicator={false}
+        ListFooterComponent={()=> <Title text={`Secciones ${heroes.length}`} />}
+      />
+    </CustomView>
   );
 };
