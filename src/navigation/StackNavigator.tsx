34ca5d025f6e24665845a4635fdf62491064cead
Rinel <rineliniguezsosa@gmail.com>
2024-10-12 16:31:31 -0600

    [FEAT] Estableciendo en el stack navigator el componente CustomSectionListScreen

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -8,6 +8,7 @@ import { SwitchScreen } from '../screens/switches/SwitchScreen';
 import { AlertScreen } from '../screens/alerts/AlertScreen';
 import { TextInputScreen } from '../screens/inputs/TextInput';
 import { PullToRefreshScreen } from '../screens/refresh/PullToRefreshScreen';
+import { CustomSectionListScreen } from '../screens/sectionlist/CustomSectionListScreen';
 
 const Stack = createStackNavigator();
 
@@ -27,6 +28,7 @@ export const StackNavigator = () => {
       <Stack.Screen name="AlertScreen" component={AlertScreen}/>
       <Stack.Screen name="TextInputScreen" component={TextInputScreen}/>
       <Stack.Screen name="PullToRefreshScreen" component={PullToRefreshScreen}/>
+      <Stack.Screen name="CustomSectionListScreen" component={CustomSectionListScreen}/>
     </Stack.Navigator>
   );
 };
