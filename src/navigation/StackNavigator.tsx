27f1a699a78640aa5a6f1f4c4b5dc7fd953571ba
Rinel <rineliniguezsosa@gmail.com>
2024-10-11 21:07:53 -0600

    [FEAT] Estableciendo en el stack navigator la ruta del TextInputScreen

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -6,6 +6,7 @@ import { Animation102BounceScreen } from '../screens/animations/Animation102Boun
 import { Animation102Screen } from '../screens/animations/Animation102Screen';
 import { SwitchScreen } from '../screens/switches/SwitchScreen';
 import { AlertScreen } from '../screens/alerts/AlertScreen';
+import { TextInputScreen } from '../screens/inputs/TextInput';
 
 const Stack = createStackNavigator();
 
@@ -23,6 +24,7 @@ export const StackNavigator = () => {
       <Stack.Screen name="Animation102Screen" component={Animation102Screen}/>
       <Stack.Screen name="SwitchScreen" component={SwitchScreen}/>
       <Stack.Screen name="AlertScreen" component={AlertScreen}/>
+      <Stack.Screen name="TextInputScreen" component={TextInputScreen}/>
     </Stack.Navigator>
   );
 };
