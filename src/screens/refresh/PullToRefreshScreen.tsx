db95e1ce10be963dcf152322811a424b57ad1c1b
Rinel <rineliniguezsosa@gmail.com>
2024-10-12 16:18:58 -0600

    [FEAT] Cambios en el archivo PullToRefreshScreen
    - importando el hook useState
    - creando el state refresh inicializado en false
    - creando el método onRefresh para actualizar el state del componente RefreshControl, estableci un setTimeout para actualizar el state en dos segundos
    - estableciendo en la prop refresing el valor del state refresh y estableciendo la prop onRefresh con la referencia del método onRefresh

--- src/screens/refresh/PullToRefreshScreen.tsx
+++ src/screens/refresh/PullToRefreshScreen.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView,RefreshControl } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Title } from '../../components/ui/Title';
 import { CustomView } from '../../components/ui/CustomView';
 
 export const PullToRefreshScreen = () => {
+  const [refresh, setRefresh] = useState(false);
   const { top } = useSafeAreaInsets();
+
+  const onRefresh = () => {
+    setTimeout(()=>{
+      setRefresh(true);
+    },2000);
+  };
   return (
     <ScrollView
-      refreshControl={<RefreshControl refreshing={true} progressViewOffset={top}/>}
+      refreshControl={<RefreshControl refreshing={refresh} progressViewOffset={top} onRefresh={onRefresh}/>}
     >
       <CustomView>
         <Title safe text="PullToRefreshScreen" />
