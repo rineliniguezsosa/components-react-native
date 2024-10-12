32693d26dc3171578c4fd7ab4cbd7e34f61a8bc5
Rinel <rineliniguezsosa@gmail.com>
2024-10-11 21:50:16 -0600

    [FEAT] Cambios en el TextInput
    - importando el useForm
    - desestructurando el form y la referencia de handleChange del customhook useForm y inicializando el valor del useForm mediante un objeto con la prop nombre vacia
    - estableciendo en el TextInput la prop value igual a form.nombre y estableciendo la prop onChangeText con la referencia de la función handleChange para escuchar los cambios el input

--- src/screens/inputs/TextInput.tsx
+++ src/screens/inputs/TextInput.tsx
@@ -4,8 +4,12 @@ import { Title } from '../../components/ui/Title';
 import { globalStyles } from '../../styles/theme';
 import { Card } from '../../components/ui/Card';
 import { TextInput } from 'react-native';
+import { useForm } from '../../hooks/useForm';
 
 export const TextInputScreen = () => {
+    const {form,handleChange} = useForm({
+        nombre:'',
+    });
   return (
     <CustomView margin>
       <Title safe text="TextInput"/>
@@ -16,6 +20,8 @@ export const TextInputScreen = () => {
         placeholder="Nombre completo"
         autoCapitalize="words"
         autoCorrect={false}
+        value={form.nombre}
+        onChangeText={(text) => handleChange('nombre',text)}
         />
       </Card>
     </CustomView>
