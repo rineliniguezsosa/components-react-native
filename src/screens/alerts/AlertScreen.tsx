db3468b66c7d76617b2748f36efd4e0cd31af573
Rinel <rineliniguezsosa@gmail.com>
2024-10-11 18:15:32 -0600

    [FEAT] Cambios en el AlertScreen
    - importando los componentes CustomView y Title
    - importando globalStyles
    - estableciendo el component CustomView con la prop style con la clase de globalMargin
    - estableciendo el component Title con las props safe y la prop text con el titulo de Alertas

--- src/screens/alerts/AlertScreen.tsx
+++ src/screens/alerts/AlertScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View,Text } from 'react-native';
+import { CustomView } from '../../components/ui/CustomView';
+import { Title } from '../../components/ui/Title';
+import { globalStyles } from '../../styles/theme';
 
 export const AlertScreen = () => {
   return (
-    <View>
-      <Text>AlertScreen</Text>
-    </View>
+    <CustomView style={globalStyles.globalMargin}>
+        <Title safe text="Alertas"/>
+    </CustomView>
   );
 };
