606c7bbd4290613d41fd7bd53cfda948789bc7f8
Rinel <rineliniguezsosa@gmail.com>
2024-10-13 14:20:49 -0600

    [FEAT] Cambios en el archivo FadeImage
    - importando el hook useState
    - importando el hook useAnimation
    - desestructurando del hook useAnimation la función fadeIn y la referencia de animatedOpacity
    - creando el hook useState con la variable isLoading inicializada true
    - estableciendo el condicional rendering para mostrar el ActivityIndicador
    - estableciendo formato en el component Animated.Image  y estableciendo la prop onLoadEnd para mostrar casi al final la animación

--- src/components/ui/FadeImage.tsx
+++ src/components/ui/FadeImage.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useState } from 'react';
 import { View,ActivityIndicator,Animated, StyleProp,ImageStyle } from 'react-native';
+import { useAnimation } from '../../hooks/useAnimated';
 
 interface Props {
     uri:string,
@@ -8,11 +9,19 @@ interface Props {
 }
 
 export const FadeImage = ({uri,style}:Props) => {
+  const {animatedOpacity, fadeIn} = useAnimation();
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <View style={{justifyContent:'center',alignItems:'center'}}>
-      <ActivityIndicator size={30} color={'gray'} style={{position:'absolute'}}/>
+      {isLoading && (<ActivityIndicator size={30} color={'gray'} style={{position:'absolute'}}/>)}
 
-      <Animated.Image source={{uri:uri}} style={[style]}/>
+      <Animated.Image
+        source={{uri:uri}}
+        onLoadEnd={()=>{
+          fadeIn({});
+          setIsLoading(false);
+        }}
+        style={[style,{opacity:animatedOpacity}]}/>
     </View>
   );
 };
