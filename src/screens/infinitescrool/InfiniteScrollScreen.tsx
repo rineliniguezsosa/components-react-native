8385ff75cf63e0f69037444f9d7f4728c7e03cd4
Rinel <rineliniguezsosa@gmail.com>
2024-10-12 21:26:28 -0600

    [FEAT] Multiples cambios en el archivo  InfiniteScrollScreen
    - deshabilitando los styles de linea
    - importando el useState
    - removiendo la importación de View e importando el FlatList
    - importando el CustomView
    - importando el Title
    - importando los colors
    - creando el state numbers inicializado por un array de números del 0 al 6
    -creando el método loadMore donde este se ejecutara al 60% para crear nuevos items, creando el newArray donde mediante el Array.from vamos a crear un nuevo array de longitud de 6 y le vamos a estar concatenando de la longitud del array  el indice
    - usamos el método setNumbers para propagar los valores anteriores y nuevos
    - estableciendo el component Customview
    - estableciendo el component Title
    - estableciendo el component FlatList donde pasamos las props data igual a numbers como el state, pasamos la prop onEndReached donde le pasamos la referencia del método onLoad para que cuando este al 60% actualizaremos los items
    - establecemos la prop onEndReachedThreshold para actualizar al 60%, establecemos la prop KeyExtractor para mostrar el id ó item unico, establecemos el renderItem para mostrar el contenido del FlatList

--- src/screens/infinitescrool/InfiniteScrollScreen.tsx
+++ src/screens/infinitescrool/InfiniteScrollScreen.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import { View,Text } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { Text,FlatList } from 'react-native';
+import { CustomView } from '../../components/ui/CustomView';
+import { Title } from '../../components/ui/Title';
+import { colors } from '../../styles/colors';
 
 export const InfiniteScrollScreen = () => {
+    const [numbers, setNumbers] = useState([0,1,2,3,4,5,6]);
+    console.log(setNumbers);
+
+    const loadMore = () => {
+        console.log('this is the end');
+        const newArray = Array.from({length:6},(_,i) => numbers.length + i);
+        setNumbers([...numbers,...newArray]);
+    };
   return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
+    <CustomView>
+      <Title safe text="InfiniteScrollScreen"/>
+
+      <FlatList
+        data={numbers}
+        onEndReached={loadMore} //cuando llega al final
+        onEndReachedThreshold={0.6}
+        keyExtractor={(item) => item.toString()}
+        renderItem={({item})=>(
+            <Text  style={{
+                height:300,
+                backgroundColor:colors.primary,
+                fontSize:50,
+                color:'white',
+            }}>{item}</Text>
+        )}
+      />
+    </CustomView>
   );
 };
