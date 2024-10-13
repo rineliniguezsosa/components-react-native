41ad883c17e8d2756345b5c06401f26828f96ea8
Rinel <rineliniguezsosa@gmail.com>
2024-10-12 21:54:06 -0600

    [FEAT] Creando el funcional component FadeImage

---
+++ src/components/ui/FadeImage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const FadeImage = () => {
+  return (
+    <View>
+      <Text>FadeImage</Text>
+    </View>
+  );
+};
