e83d3266dc297aa14407f2070533c5d76bc73531
Rinel <rineliniguezsosa@gmail.com>
2024-10-12 20:21:09 -0600

    [FEAT] Multiples cambios en el archivo ModalScreen
    - importando el useState
    - eliminando la importación de Text
    - importando Modal y Platform
    - importando CustomView
    - importando Title
    - importando Button
    - creeando el state visible inicializado en false
    - creando la función openModal para actualizar el state visible a true
    - creando la función closeModal para actualizar el state visible a false
    - estableciendo el component CustomVisible con la prop margin
    - estableciendo el component Title con la prop safe y el text igual ModalScreen
    - estableciendo el component Modal con la prop visible igual a visible del state  y estableciendo el tipo de animación del modal a slide
    - establiendo el contenido por mostrar donde lo más importando fue agregar el botón para cerrar el modal

--- src/screens/modal/ModalScreen.tsx
+++ src/screens/modal/ModalScreen.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View,Text } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { View,Modal, Platform } from 'react-native';
+import { CustomView } from '../../components/ui/CustomView';
+import { Title } from '../../components/ui/Title';
+import { Button } from '../../components/ui/Button';
 
 export const ModalScreen = () => {
+  const [visible, setVisible] = useState(false);
+
+  const openModal = () =>{
+    setVisible(true);
+  };
+
+  const closeModal = () =>{
+    setVisible(false);
+  };
   return (
-    <View>
-      <Text>ModalScreen</Text>
-    </View>
+    <CustomView margin>
+      <Title safe text="ModalScreen"/>
+
+      <Modal
+        visible={visible}
+        animationType="slide"
+      >
+        <View style={{flex:1,backgroundColor:'rgba(0,0,0,0)'}}>
+          <View style={{paddingHorizontal:10}}>
+            <Title text="Modal"/>
+          </View>
+
+          <View style={{flex:1}} />
+
+          <Button style={{height:Platform.OS === 'android' ? 40 : 60,borderRadius:0}} text="Close modal" onPress={closeModal}/>
+        </View>
+      </Modal>
+
+      <Button
+        text="open modal"
+        onPress={openModal}
+      />
+    </CustomView>
   );
 };
