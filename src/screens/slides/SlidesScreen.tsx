55483aa6b5e2a6a4b90d75af060d7a3e4fea0c00
Rinel <rineliniguezsosa@gmail.com>
2025-12-17 13:38:56 -0600

    [FEAT] Cambios en el archivo SlidesScreen
    - importando ImageSourcePropType
    - creando la interface Slide para tipar las props title,desc como string y img como ImageSourcePropType para resolver la imagen que contiene propiedades como la uri, scale, width y height etc
    - estableciendo el array de objetos items

--- src/screens/slides/SlidesScreen.tsx
+++ src/screens/slides/SlidesScreen.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text, ImageSourcePropType } from 'react-native';
+
+interface Slide {
+  title: string;
+  desc: string;
+  img: ImageSourcePropType;
+}
+
+const items: Slide[] = [
+  {
+    title: 'Titulo 1',
+    desc: 'Ea et eu enim fugiat sunt reprehenderit sunt aute quis tempor ipsum cupidatat et.',
+    img: require('../../assets/slide-1.png'),
+  },
+  {
+    title: 'Titulo 2',
+    desc: 'Anim est quis elit proident magna quis cupidatat curlpa labore Lorem ea. Exercitation mollit velit in aliquip tempor occaecat dolor minim amet dolor enim cillum excepteur. ',
+    img: require('../../assets/slide-2.png'),
+  },
+  {
+    title: 'Titulo 3',
+    desc: 'Ex amet duis amet nulla. Aliquip ea Lorem ea culpa consequat proident. Nulla tempor esse ad tempor sit amet Lorem. Velit ea labore aute pariatur commodo duis veniam enim.',
+    img: require('../../assets/slide-3.png'),
+  },
+];
 
 export const SlidesScreen = () => {
   return (
