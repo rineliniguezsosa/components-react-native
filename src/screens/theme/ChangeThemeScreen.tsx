f8188aba613ee370390cdb5dba08d89f2c73558b
Rinel <rineliniguezsosa@gmail.com>
2025-12-17 23:50:21 -0600

    [FEAT] Creando el funcional component ChangeThemeScreen

---
+++ src/screens/theme/ChangeThemeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const ChangeThemeScreen = () => {
+  return (
+    <View>
+      <Text>ChangeThemeScreen</Text>
+    </View>
+  );
+};
