3b9d7ae8b37beb4d7e8d3430a4c1574fdb770f12
Rinel <rineliniguezsosa@gmail.com>
2025-12-19 00:46:50 -0600

    [FEAT] Cambios en el componente ChangeThemeScreen
    - importamos useContext de react para consumir el contexto
    - importamos ThemeContext para usar el contexto
    - desestructuramos setTheme función de useContext de ThemeContext
    - cambios los temas mediante setTheme en los métodos de los botones

--- src/screens/theme/ChangeThemeScreen.tsx
+++ src/screens/theme/ChangeThemeScreen.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { CustomView,Title,Button } from '../../components';
 import { View } from 'react-native';
+import { ThemeContext } from '../../context/ThemeContext';
 
 
 export const ChangeThemeScreen = () => {
+  const { setTheme } = useContext(ThemeContext)
   return (
     <CustomView margin>
       <Title text="Cambiar el tema" safe/>
 
       <Button
         text="Dark"
-        onPress={() => {}}
+        onPress={() => setTheme('dark')}
       />
 
       <View style={{marginTop:10}}/>
 
       <Button
         text="Light"
-        onPress={() => {}}
+        onPress={() => setTheme('light')}
       />
     </CustomView>
   );
