e78e13b7e1cc69e3a50600373f42d16f50370ef4
Rinel <rineliniguezsosa@gmail.com>
2025-12-19 12:34:02 -0600

    [FEAT] Cambios en ChangeThemeScreen
    - importando Text de react native
    - desestructurando colors de useContext
    - estableciendo un View con un margin top de 10 como un separador
    - estableciendo el componente Text donde estableci un json.stringify para ver el objeto colors , separación de 2
    - prop style para establecer el color del texto

--- src/screens/theme/ChangeThemeScreen.tsx
+++ src/screens/theme/ChangeThemeScreen.tsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { CustomView,Title,Button } from '../../components';
-import { View } from 'react-native';
+import { View,Text } from 'react-native';
 import { ThemeContext } from '../../context/ThemeContext';
 
 
 export const ChangeThemeScreen = () => {
-  const { setTheme, currentTheme } = useContext(ThemeContext)
+  const { setTheme, currentTheme,colors } = useContext(ThemeContext)
   return (
     <CustomView margin>
       <Title text={`Cambiar el tema: ${currentTheme}`} safe/>
@@ -21,6 +21,10 @@ export const ChangeThemeScreen = () => {
         text="Light"
         onPress={() => setTheme('light')}
       />
+
+      <View style={{marginTop:10}}/>
+
+       <Text style={{color:colors.text}}>{JSON.stringify(colors,null,2)}</Text>
     </CustomView>
   );
 };
