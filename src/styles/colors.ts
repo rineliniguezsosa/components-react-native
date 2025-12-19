2f17b0c9e4d18ef91a91e9059d2ac5dcb7a4c8a8
Rinel <rineliniguezsosa@gmail.com>
2025-12-19 00:21:05 -0600

    [FEAT] Creando el objeto darkcolors y lightcolors, estos son de tipo ThemeColors para definir el cuerpo de nuestros temas

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -13,3 +13,19 @@ export const colors: ThemeColors = {
     cardBackground: 'white',
     buttonTextColor: 'white',
 };
+
+export const darkcolors: ThemeColors = {
+    primary: '#5856D6',
+    text: 'white',
+    background: '#090909',
+    cardBackground: '#2D2D2D',
+    buttonTextColor: 'white',
+};
+
+export const lightcolors: ThemeColors = {
+    primary: '#5856D6',
+    text: 'black',
+    background: '#F3F2F7',
+    cardBackground: 'white',
+    buttonTextColor: 'white',
+};
