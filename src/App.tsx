8939007af89a08386f44f481d7ff5f01f7ea6669
Rinel <rineliniguezsosa@gmail.com>
2025-12-19 00:41:13 -0600

    [FEAT] Cambios en el archivo App.tsx
    - importanto PropsWithChildren  para definir ó tipar un nodo
    - importando ThemeProvider para proveer la data a compartir
    - creamos la función AppState para poder compartirla en la función App ya que solo devolvemos un nodo
    - envolvemos AppState en el StackNavigator para poder compartir la data entre los nodos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './navigation/StackNavigator';
+import { ThemeProvider } from './context/ThemeContext';
+
+const AppState = ({children}:PropsWithChildren) => {
+  return (
+    <NavigationContainer>
+      <ThemeProvider>
+        {children}
+      </ThemeProvider>
+    </NavigationContainer>
+  )
+}
 
 export const App = () => {
   return (
-    <NavigationContainer>
+    <AppState>
       <StackNavigator/>
-    </NavigationContainer>
+    </AppState>
   );
 };
