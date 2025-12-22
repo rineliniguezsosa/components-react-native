a80d450184ebe31d48c9f1cc05ed1507ebf8ac04
Rinel <rineliniguezsosa@gmail.com>
2025-12-22 14:58:14 -0600

    [FEAT] Cambios en el componente CustomSwitch
    - importando useContext
    - comentando colors de styles/colors
    - importando ThemeContext
    - desestructurando colors de ThemeContext
    - estableciendo el color de background mediante colors.cardBackground considerando el tema ligt ó dark

--- src/components/ui/CustomSwitch.tsx
+++ src/components/ui/CustomSwitch.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View,Text, StyleSheet, Switch, Platform } from 'react-native';
-import { colors } from '../../styles/colors';
+// import { colors } from '../../styles/colors';
+import { ThemeContext } from '../../context/ThemeContext';
 
 interface Props {
     isOn:boolean,
@@ -8,8 +9,9 @@ interface Props {
     onChange:(value:boolean)=> void,
 }
 export const CustomSwitch = ({isOn,text,onChange}:Props) => {
+  const { colors } = useContext(ThemeContext);
   return (
-    <View style={styles.switchRow}>
+    <View style={[styles.switchRow,{backgroundColor: colors.cardBackground}]}>
       {text && (<Text style={{color:colors.text}}>CustomSwitch</Text>)}
       <Switch
         trackColor={{false: '#767577', true: '#81b0ff'}}
