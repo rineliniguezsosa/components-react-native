e3a1708949c2209eace1b143db969b235a356cb9
Rinel <rineliniguezsosa@gmail.com>
2025-12-22 14:35:16 -0600

    [FEAT] Multiples cambios en el Homescreen
    - comentando la importación de useContext
    - importando el CustomView
    - comentando la importación de ThemeContext
    - comentando la desestructuración de colors
    - estableciendo formato en el componente de MenuItemList y estableciendo las props isFirst y isLast para añadirles valores booleanos
    - envolviendo el componente CustomView por el contenido de HomeScreen

--- src/screens/home/Homescreen.tsx
+++ src/screens/home/Homescreen.tsx
@@ -1,23 +1,32 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+// import React, { useContext } from 'react';
 import { View, ScrollView } from 'react-native';
 import { globalStyles } from '../../styles/theme';
 import { Title } from '../../components/ui/Title';
 import { menuItems } from '../../helpers/menu.items';
 import { MenuItemList } from '../../components/ui/MenuItemList';
+import { CustomView } from '../../components';
+// import { ThemeContext } from '../../context/ThemeContext';
 
 export const HomeScreen = () => {
+  // const { colors } = useContext(ThemeContext);
   return (
+    <CustomView>
     <View style={[globalStyles.mainContainer]}>
       <View style={globalStyles.globalMargin}>
         <ScrollView>
           <View style={{marginTop:30}}/>
           <Title text="Opciones de menú" safe/>
-          {menuItems.map((item) => (
-            <MenuItemList key={item.component} {...item}/>
+          {menuItems.map((item,index) => (
+            <MenuItemList 
+            key={item.component}
+            {...item} 
+            isFirst={index === 0}
+            isLast={index === menuItems.length - 1}/>
           ))}
         </ScrollView>
       </View>
     </View>
+    </CustomView>
   );
 };
