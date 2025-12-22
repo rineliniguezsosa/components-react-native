bb1280f832d0022bab932215c8be55242eefd883
Rinel <rineliniguezsosa@gmail.com>
2025-12-22 14:46:02 -0600

    [FEAT] Cambios en el MenuItemList
    - importando iseContext para consumir el contexto
    - comentando colors de styles/colors
    - importando ThemeContext para poder consumirlo
    - estableciendo a la interface Props los atributos isFisrt y isLast de tipo boolean y opcionales
    - estableciendo en la función MenuItemList la desestructuración de las props isFirst y isLast y inicializandolas en false
    - desestructurando colors del ThemeContext
    - estableciendo formato en la prop style
    - estableciendo atributos css para añadirles a las cards de nuestra lista principal
    - cambiando el color del icono para que sea establecido dependiendo del tema light ó dark
    - cambiando el color del icono "chevron-forward-outline" dependiendo del tema light ó dark

--- src/components/ui/MenuItemList.tsx
+++ src/components/ui/MenuItemList.tsx
@@ -1,25 +1,42 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useContext } from 'react';
 import { View,Text,Pressable,StyleSheet } from 'react-native';
-import { colors } from '../../styles/colors';
+// import { colors } from '../../styles/colors';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from '@react-navigation/native';
+import { ThemeContext } from '../../context/ThemeContext';
 
 interface Props {
     name:string,
     icon:string,
-    component:string
+    component:string,
+    isFirst?: boolean,
+    isLast?: boolean
 }
 
-export const MenuItemList = ({name,icon,component}:Props) => {
+export const MenuItemList = ({name,icon,component,isFirst = false,isLast = false}:Props) => {
   const navigation = useNavigation<any>();
+  const { colors } = useContext(ThemeContext)
   return (
     <>
     <Pressable onPress={()=> navigation.navigate(component)}>
-      <View style={{...styles.constainer,backgroundColor:colors.background}}>
-        <Icon name={icon} size={25} style={{marginRight:10,color:colors.primary}}/>
+      <View style={{
+        ...styles.constainer,
+        backgroundColor:colors.background,
+        ...(isFirst && {
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+              paddingTop: 10,
+        }),
+        ...(isLast && {
+              borderBottomLeftRadius: 10,
+              borderBottomRightRadius: 10,
+              paddingBottom: 10,
+        }),
+      }}>
+        <Icon name={icon} size={25} style={{marginRight:10,color:colors.text}}/>
         <Text style={{color:colors.text}}>{name}</Text>
-        <Icon name="chevron-forward-outline" size={25} style={{marginLeft:'auto',color:colors.primary}}/>
+        <Icon name="chevron-forward-outline" size={25} style={{marginLeft:'auto',color:colors.text}}/>
       </View>
     </Pressable>
     </>
