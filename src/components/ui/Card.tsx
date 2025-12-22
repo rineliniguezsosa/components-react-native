a32c69ad4d1e3000b9d072a419c1344be7b9b88d
Rinel <rineliniguezsosa@gmail.com>
2025-12-22 14:54:59 -0600

    [FEAT] Cambios en el componente Card
    - importando useContext
    - comentando colors de styles/colors
    - importando ThemeContext
    - desestructurando colors de ThemeContext para definir el color dependiente del tema que deseé el usuario

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useContext } from 'react';
 import { View, StyleProp, ViewStyle } from 'react-native';
-import { colors } from '../../styles/colors';
+// import { colors } from '../../styles/colors';
+import { ThemeContext } from '../../context/ThemeContext';
 
 interface Props extends PropsWithChildren {
     style?:StyleProp<ViewStyle>
 }
 
 export const Card = ({style,children}:Props) => {
+  const { colors } = useContext(ThemeContext);
   return (
     <View style={[{backgroundColor:colors.cardBackground,borderRadius:10,padding:10},style]}>
       {children}
