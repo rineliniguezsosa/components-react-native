924bb2e92054f1b53587879d2f5394351993961a
Rinel <rineliniguezsosa@gmail.com>
2025-12-23 13:42:17 -0600

    [FEAT] Cambios en CustomSectionListScreen
    - importando useContext
    - comentando colors de styles/colors
    - importando ThemeContext
    - desestructurando colors de ThemeContext
    - estableciendo la prop color con el color dinamico del componente Text

--- src/screens/sectionlist/CustomSectionListScreen.tsx
+++ src/screens/sectionlist/CustomSectionListScreen.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, SectionList } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
 import { Title } from '../../components/ui/Title';
 import { heroes } from '../../helpers/heroes';
 import { globalStyles } from '../../styles/theme';
-import { colors } from '../../styles/colors';
+// import { colors } from '../../styles/colors';
+import { ThemeContext } from '../../context/ThemeContext';
 
 export const CustomSectionListScreen = () => {
+  const { colors } = useContext(ThemeContext);
   return (
     <CustomView margin>
       <Title safe text="CustomSectionListScreen"/>
@@ -16,10 +18,10 @@ export const CustomSectionListScreen = () => {
         sections={heroes}
         keyExtractor={item => item}
         renderItem={({item})=> (
-            <Text style={{marginVertical:2}}>{item}</Text>
+            <Text style={{marginVertical:2, color: colors.text}}>{item}</Text>
         )}
         renderSectionHeader={(item) => (
-                <Text style={{...globalStyles.subTitle,backgroundColor:colors.background}}>
+                <Text style={{...globalStyles.subTitle,backgroundColor:colors.background, color: colors.text}}>
                     {item.section.title}
                 </Text>
         )}
